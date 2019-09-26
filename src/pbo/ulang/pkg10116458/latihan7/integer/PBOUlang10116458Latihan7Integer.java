521c3e9d158a73fc149029b6b31afefd15964749
nanda <nanda@nanda>
2019-09-26 20:43:12 +0700

    commit men

---
+++ src/pbo/ulang/pkg10116458/latihan7/integer/PBOUlang10116458Latihan7Integer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10116458.latihan7.integer;
+
+/**
+ *
+ * @author
+ * NAMA     : Rinanda Al Qorrie Akbar
+ * NIM      : 10116458
+ */
+public class PBOUlang10116458Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
